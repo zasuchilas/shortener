8db55c1372b10fde5e8c3189d117e8cc982c774d
zasuchilas <zasuchilas@ya.ru>
2024-09-01 12:50:58 +0300

    Tests refactoring

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ URL shortening service
 Commands:  
 ```shell
 go test -v ./...
+go clean -testcache
+
 go run ./cmd/shortener/
 go build -o shortener
 ./shortener -a ":8033" -b ":8034"
--- internal/app/server/server_test.go
+++ internal/app/server/server_test.go
@@ -5,177 +5,94 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/zasuchilas/shortener/internal/app/storage"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 )
 
-//func TestServer_writeURLHandler(t *testing.T) {
-//	type want struct {
-//		statusCode  int
-//		contentType string
-//		response    string
-//	}
-//
-//	tests := []struct {
-//		name   string
-//		method string
-//		target string
-//		body   string
-//		want   want
-//	}{
-//		{
-//			name:   "positive test #1",
-//			method: http.MethodPost,
-//			target: "/",
-//			body:   "http://спорт.ru/",
-//			want: want{
-//				statusCode:  201,
-//				contentType: "text/plain",
-//				response:    "http://localhost:8080/", // http://localhost:8080/LcPCiANk
-//			},
-//		},
-//		{
-//			name:   "negative test #1 (has many different errors)",
-//			method: http.MethodGet,
-//			target: "/abc",
-//			body:   "http://спор т.ru/",
-//			want: want{
-//				statusCode:  400,
-//				contentType: "text/plain",
-//				response:    "", // parse "http://спор т.ru/": invalid character " " in host name
-//			},
-//		},
-//	}
-//
-//	st := storage.New()
-//	srv := New("localhost:8080", st)
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			r := httptest.NewRequest(tt.method, tt.target, strings.NewReader(tt.body))
-//			w := httptest.NewRecorder()
-//			srv.writeURLHandler(w, r)
-//			res := w.Result()
-//
-//			// checking status code
-//			assert.Equal(t, tt.want.statusCode, res.StatusCode)
-//
-//			// checking content type
-//			assert.Contains(t, res.Header.Get("Content-Type"), tt.want.contentType)
-//
-//			// checking body content
-//			defer res.Body.Close()
-//			resBody, err := io.ReadAll(res.Body)
-//			require.NoError(t, err)
-//			assert.Contains(t, string(resBody), tt.want.response)
-//		})
-//	}
-//}
+var (
+	ts *httptest.Server
+	st *storage.Database
+)
 
-//func TestServer_readURLHandler(t *testing.T) {
-//	type want struct {
-//		statusCode  int
-//		contentType string
-//		location    string
-//	}
-//
-//	tests := []struct {
-//		name   string
-//		method string
-//		target string
-//		want   want
-//	}{
-//		{
-//			name:   "positive test #1",
-//			method: http.MethodGet,
-//			target: "http://localhost:8080/abcdefgh",
-//			want: want{
-//				statusCode:  307,
-//				contentType: "text/plain",
-//				location:    "http://спорт.ru/",
-//			},
-//		},
-//		{
-//			name:   "negative test #1 (has many different errors)",
-//			method: http.MethodGet,
-//			target: "/abc",
-//			want: want{
-//				statusCode:  400,
-//				contentType: "text/plain",
-//				location:    "",
-//			},
-//		},
-//	}
-//
-//	st := &storage.Database{
-//		Urls: make(map[string]string),
-//		Hash: make(map[string]string),
-//	}
-//	srv := New("localhost:8080", st)
-//
-//	st.Urls["http://спорт.ru/"] = "abcdefgh"
-//	st.Hash["abcdefgh"] = "http://спорт.ru/"
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			r := httptest.NewRequest(tt.method, tt.target, nil)
-//			w := httptest.NewRecorder()
-//			srv.readURLHandler(w, r)
-//			res := w.Result()
-//			defer res.Body.Close()
-//
-//			// checking status code
-//			assert.Equal(t, tt.want.statusCode, res.StatusCode)
-//
-//			// checking content type
-//			assert.Contains(t, res.Header.Get("Content-Type"), tt.want.contentType)
-//
-//			// checking location header
-//
-//			assert.Equal(t, res.Header.Get("Location"), tt.want.location)
-//			//if tt.want.location != "" {
-//			//	assert.NotEmpty(t, res.Header.Get("Location"))
-//			//} else {
-//			//	assert.Empty(t, res.Header.Get("Location"))
-//			//}
-//		})
-//	}
-//}
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
 
-func TestRouter(t *testing.T) {
-	st := &storage.Database{
+func setup() {
+	log.Println("setup started")
+
+	st = &storage.Database{
 		Urls: make(map[string]string),
 		Hash: make(map[string]string),
 	}
+	refillDatabase()
+
+	srv := New(st)
+	ts = httptest.NewServer(srv.Router())
+
+	log.Println("setup completed")
+}
+
+func teardown() {
+	ts.Close()
+	log.Println("teardown completed")
+}
+
+func refillDatabase() {
 	st.Urls["http://спорт.ru/"] = "abcdefgh"
 	st.Hash["abcdefgh"] = "http://спорт.ru/"
-	srv := New(st)
+}
 
-	ts := httptest.NewServer(srv.Router())
-	defer ts.Close()
+func testRequest(t *testing.T, ts *httptest.Server, method,
+	path string, body io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	require.NoError(t, err)
 
-	// (1) testing writeURLHandler
-	type want1 struct {
+	client := ts.Client()
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Logf("ERR %s", err.Error())
+	}
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, string(respBody)
+}
+
+func TestServer_writeURLHandler(t *testing.T) {
+	type want struct {
 		statusCode  int
 		contentType string
 		response    string
 	}
 
-	tests1 := []struct {
+	tests := []struct {
 		name   string
 		method string
 		target string
 		body   string
-		want   want1
+		want   want
 	}{
 		{
 			name:   "positive test #1",
 			method: http.MethodPost,
 			target: "/",
 			body:   "http://спорт.ru/",
-			want: want1{
+			want: want{
 				statusCode:  201,
 				contentType: "text/plain",
 				response:    "abcdefgh", // "http://localhost:8080/", // http://localhost:8080/LcPCiANk
@@ -186,7 +103,7 @@ func TestRouter(t *testing.T) {
 			method: http.MethodGet,
 			target: "/abc",
 			body:   "http://спор т.ru/",
-			want: want1{
+			want: want{
 				statusCode:  400,
 				contentType: "text/plain",
 				response:    "", // parse "http://спор т.ru/": invalid character " " in host name
@@ -194,7 +111,7 @@ func TestRouter(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests1 {
+	for _, tt := range tests {
 
 		t.Run(tt.name, func(t *testing.T) {
 			res, resBody := testRequest(t, ts, tt.method, tt.target, strings.NewReader(tt.body))
@@ -210,9 +127,11 @@ func TestRouter(t *testing.T) {
 			assert.Contains(t, resBody, tt.want.response)
 		})
 	}
+}
 
-	// (2) testing readURLHandler
-	type want2 struct {
+func TestServer_readURLHandler(t *testing.T) {
+
+	type want struct {
 		statusCode  int
 		contentType string
 		location    string
@@ -222,13 +141,13 @@ func TestRouter(t *testing.T) {
 		name   string
 		method string
 		target string
-		want   want2
+		want   want
 	}{
 		{
 			name:   "positive test #1",
 			method: http.MethodGet,
 			target: "/abcdefgh",
-			want: want2{
+			want: want{
 				statusCode:  307,
 				contentType: "text/plain",
 				location:    "http://спорт.ru/",
@@ -238,7 +157,7 @@ func TestRouter(t *testing.T) {
 			name:   "negative test #1 (has many different errors)",
 			method: http.MethodGet,
 			target: "/abc",
-			want: want2{
+			want: want{
 				statusCode:  400,
 				contentType: "text/plain",
 				location:    "",
@@ -270,27 +189,4 @@ func TestRouter(t *testing.T) {
 			//}
 		})
 	}
-}
-
-func testRequest(t *testing.T, ts *httptest.Server, method,
-	path string, body io.Reader) (*http.Response, string) {
-	req, err := http.NewRequest(method, ts.URL+path, body)
-	require.NoError(t, err)
-
-	client := ts.Client()
-	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Logf("ERR %s", err.Error())
-	}
-	require.NoError(t, err)
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-
-	return resp, string(respBody)
 }
