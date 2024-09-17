eacdc11a3ed864714add0f6fbca433a658f348aa
zasuchilas <zasuchilas@ya.ru>
2024-09-17 16:18:08 +0300

    Added WriteURLs func fo postgresql

--- internal/app/storage/dbpgsql/dbpgsql.go
+++ internal/app/storage/dbpgsql/dbpgsql.go
@@ -37,7 +37,7 @@ func (d *DBPgsql) Stop() {
 	}
 }
 
-func (d *DBPgsql) WriteURL(ctx context.Context, origURL string) (shortURL string, was bool, err error) {
+func (d *DBPgsql) WriteURL_(ctx context.Context, origURL string) (shortURL string, was bool, err error) {
 
 	ctxTm, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
@@ -91,7 +91,7 @@ func (d *DBPgsql) WriteURL(ctx context.Context, origURL string) (shortURL string
 	logger.Log.Debug("closing tx")
 	err = tx.Commit()
 	if err != nil {
-		logger.Log.Debug("closing tx exception (if we get this error tx will rollback ?)", zap.Error(err))
+		logger.Log.Debug("closing tx exception (if we get this error tx will must rollback)", zap.Error(err))
 		return "", false, err
 	}
 
@@ -102,6 +102,89 @@ func (d *DBPgsql) WriteURL(ctx context.Context, origURL string) (shortURL string
 		return "", false, err
 	}
 	return v.ShortURL, true, nil
+}
+
+func (d *DBPgsql) WriteURL(ctx context.Context, origURL string) (shortURL string, was bool, err error) {
+	shortURLs, err := d.WriteURLs(ctx, []string{origURL})
+	if err != nil {
+		return "", false, err
+	}
+	if len(shortURLs) != 1 {
+		return "", false, errors.New("something wrong with writing URL")
+	}
+	return shortURLs[0], false, nil
+}
+
+func (d *DBPgsql) WriteURLs(ctx context.Context, origURLs []string) (shortURLs []string, err error) {
+
+	ctxTm, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	logger.Log.Debug("start tx in postgresql storage")
+	tx, err := d.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	// INSERT INTO urls (uuid, short, original) VALUES ($1, $2, $3) ... so SERIAL will break
+	// INSERT INTO urls (short, original) VALUES ($1, $2) ON CONFLICT DO NOTHING ... so urls_uuid_seq will break
+	// IT IS NECESSARY: if origURL already exists, do nothing (including not changing the urls_uuid_set counter)
+	stmt, err := tx.PrepareContext(ctx,
+		"INSERT INTO urls (short, original) "+
+			"SELECT $1, $2 "+
+			"WHERE NOT EXISTS (SELECT 1 FROM urls WHERE original = $3) "+
+			"RETURNING uuid")
+	if err != nil {
+		logger.Log.Error("preparing stmt", zap.Error(err))
+		return nil, err
+	}
+	defer stmt.Close()
+
+loop:
+	for _, origURL := range origURLs {
+		select {
+		case <-ctxTm.Done():
+			err = fmt.Errorf("the operation was canceled")
+			break loop
+		default:
+			logger.Log.Debug("getting last/next uuid from urls_uuid_seq")
+			var nextUUID int64
+			nextUUID, err = getNextUUID(ctx, d.db)
+			if err != nil {
+				logger.Log.Error("getting next uuid", zap.Error(err))
+				break loop
+			}
+			shortURLCandidate := hashfuncs.EncodeZeroHash(nextUUID)
+			logger.Log.Debug("got next shortURL candidate & next uuid",
+				zap.String("shortURLCandidate", shortURLCandidate), zap.Int64("uuid", nextUUID))
+
+			logger.Log.Debug("executing stmt")
+			_, err = stmt.ExecContext(ctx, shortURLCandidate, origURL, origURL)
+			if err != nil {
+				logger.Log.Error("executing stmt", zap.Error(err))
+				break loop
+			}
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Log.Debug("closing tx")
+	err = tx.Commit()
+	if err != nil {
+		logger.Log.Debug("closing tx exception (if we get this error tx will must rollback)", zap.Error(err))
+		return nil, err
+	}
+
+	logger.Log.Debug("getting inserted urls")
+	shortURLs, err = selectByOrigURLs(ctx, d.db, origURLs)
+	if err != nil {
+		logger.Log.Error("finding inserted url in postgresql storage (not impossible)", zap.Error(err))
+		return nil, err
+	}
+	return shortURLs, nil
 }
 
 func (d *DBPgsql) ReadURL(ctx context.Context, shortURL string) (origURL string, err error) {
--- internal/app/storage/dbpgsql/sidefuncs.go
+++ internal/app/storage/dbpgsql/sidefuncs.go
@@ -6,6 +6,7 @@ import (
 	"github.com/zasuchilas/shortener/internal/app/logger"
 	"github.com/zasuchilas/shortener/internal/app/models"
 	"go.uber.org/zap"
+	"strings"
 	"time"
 )
 
@@ -40,6 +41,50 @@ func getNextUUID(ctx context.Context, db *sql.DB) (int64, error) {
 		return lastUUID, nil
 	}
 	return lastUUID + 1, nil
+}
+
+func getNextUUIDTx(ctx context.Context, tx *sql.Tx) (int64, error) {
+	var lastUUID int64
+	var isCalled bool
+	err := tx.QueryRowContext(ctx,
+		`SELECT last_value, is_called FROM urls_uuid_seq`).Scan(&lastUUID, &isCalled)
+	if err != nil {
+		return 0, err
+	}
+	if isCalled == false {
+		return lastUUID, nil
+	}
+	return lastUUID + 1, nil
+}
+
+func selectByOrigURLs(ctx context.Context, db *sql.DB, origURLs []string) (shortURLs []string, err error) {
+
+	rows, err := db.QueryContext(ctx,
+		`SELECT short FROM urls WHERE original IN ($1)`,
+		strings.Join(origURLs, ","))
+	if err != nil {
+		logger.Log.Error("creating query", zap.Error(err))
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var shortURL string
+		err = rows.Scan(&shortURL)
+		if err != nil {
+			logger.Log.Error("scanning rows", zap.Error(err))
+			return nil, err
+		}
+		shortURLs = append(shortURLs, shortURL)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		logger.Log.Error("checkin rows on errors", zap.Error(err))
+		return nil, err
+	}
+
+	return shortURLs, nil
 }
 
 func findByOrig(ctx context.Context, db *sql.DB, origURL string) (urlRow *models.URLRow, ok bool, err error) {
